d4102e40333be37afbd107d36b9a51520df9f7ff
Eduard Glukhov <binolojo@gmail.com>
2023-03-12 16:55:40 +0300

    feat: add additional fields to polls app models

--- polls/models.py
+++ polls/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 import json
 
 class Question(models.Model):
@@ -11,6 +13,10 @@ class Question(models.Model):
             'question_text': self.question_text,
             'pub_date': self.pub_date.isoformat(timespec='milliseconds'),
         })
+    
+    def was_published_recently(self) -> bool:
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -20,7 +26,7 @@ class Choice(models.Model):
     def __str__(self) -> str:
         return json.dumps({
             'id': self.id,
-            'question': self.question,
+            'question': self.question.pk,
             'choice_text': self.choice_text,
             'votes': self.votes,
         })
