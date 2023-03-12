716776f3330d25e44a268c095cc6a2f5fad3a9df
Eduard Glukhov <binolojo@gmail.com>
2023-03-12 18:39:51 +0300

    feat: add polls app endpoints with views and templates

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path(route='',view=views.index, name='index')
+    path(route='', view=views.IndexView.as_view(), name='index'),
+    path(route='<int:pk>/', view=views.DetailView.as_view(), name='detail'),
+    path(route='<int:pk>/results/', view=views.ResultsView.as_view(), name='results'),
+    path(route='<int:pk>/vote/', view=views.vote, name='vote'),
 ]
