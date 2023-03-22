e38f8aeda1c77f4ff1f934ba964181fa527ae67e
Eduard Glukhov <binolojo@gmail.com>
2023-03-22 13:48:28 +0300

    refactor: replace 'perform_update'/'perform_delete' with 'check_object_permissions' method

--- companies/views.py
+++ companies/views.py
@@ -27,26 +27,14 @@ class RetrieveUpdateDestroyCompanyView(generics.RetrieveUpdateDestroyAPIView):
 
         return CompanyModel.objects.all().filter(members__user=user.id)
 
-    def is_user_related_to_company(self, company_id: int, role: CompanyMemberRoles = None):
+    def check_object_permissions(self, request, obj):
+        # Pass Get requests
+        if self.request.method == "GET":
+            return
+
         count = CompanyMemberModel.objects.filter(
-            user_id=self.request.user.id, company_id=company_id, role=role
+            user_id=self.request.user.id, company_id=obj.id, role=CompanyMemberRoles.ADMIN.value
         ).count()
 
         if count == 0:
             raise PermissionDenied("You don't have permission to take this action")
-
-        return True
-
-    def can_update(self, company_id: int):
-        return self.is_user_related_to_company(company_id=company_id, role=CompanyMemberRoles.ADMIN.value)
-
-    def can_destroy(self, company_id: int):
-        return self.is_user_related_to_company(company_id=company_id, role=CompanyMemberRoles.ADMIN.value)
-
-    def perform_update(self, serializer: CompanySerializer):
-        if self.can_update(company_id=serializer.instance.id):
-            return serializer.save()
-
-    def perform_destroy(self, instance: CompanyModel):
-        if self.can_destroy(company_id=instance.id):
-            return instance.delete()
